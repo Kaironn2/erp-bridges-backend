fcb4668fd9418f6ff86f7b62b26f955cdb40763e
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-07-18 13:35:04 -0300

    tests (Adapters): tests refactoreds to use conftest fixtures

--- src/reports/tests/mgt/test_adapter_buy_orders_csv.py
+++ src/reports/tests/mgt/test_adapter_buy_orders_csv.py
@@ -8,92 +8,63 @@ from mgt.schemas.buy_orders_report_schema import BuyOrderReportData
 from reports.adapters.mgt.buy_orders_csv_adapter import MgtBuyOrdersCsvAdapter
 
 
-@pytest.fixture
-def valid_csv_data() -> str:
-    """return multi line string with valid data"""
-    return (
-        'Pedido #;ID do Pedido;Firstname;Lastname;Email;Grupo do Cliente;Número CPF/CNPJ;Comprado Em;Shipping Telephone;Status;Número do Rastreador;Qtd. Vendida;Frete;Desconto;Payment Type;Total da Venda\n'  # noqa: E501
-        '507943839;201414;Luiza;Correia;luigi32@VIANA.br;VIP;091.764.823-40;10/04/2025 22:01:33;(71) 6379-4026;Cancelado;TRACK123;1;R$ 23,24;R$ 0,00;boleto;R$ 85,46\n'  # noqa: E501
-        '414797776;969693;Rodrigo;Freitas;sarahmendes@carvalho.com;Atacado;594.603.817-66;30/01/2025 22:01:33; - ;Entregue;teste;5;R$ 9,81;R$ 13,23;cartão;R$ 493,35\n'  # noqa: E501
-    )
-
-
-@pytest.fixture
-def header_only_csv_data() -> str:
-    """return buy orders csv headers"""
-    return 'Pedido #;ID do Pedido;Firstname;Lastname;Email;Grupo do Cliente;Número CPF/CNPJ;Comprado Em;Shipping Telephone;Status;Número do Rastreador;Qtd. Vendida;Frete;Desconto;Payment Type;Total da Venda\n'  # noqa: E501
-
-
-@pytest.fixture
-def data_with_missing_required_field() -> str:
-    """
-    Return csv with an empty mandatory field
-    """
-    return (
-        'Pedido #;ID do Pedido;Firstname;Lastname;Email;Grupo do Cliente;Número CPF/CNPJ;Shipping Telephone;Status;Número do Rastreador;Qtd. Vendida;Frete;Desconto;Payment Type;Total da Venda\n'  # noqa: E501
-        '507943839;201414;Luiza;Correia;email-valido@teste.com;VIP;091.764.823-40;(71) 6379-4026;Cancelado;TRACK123;1;R$ 23,24;R$ 0,00;boleto;R$ 85,46'  # noqa: E501
-    )
-
-
-def test_successful_processing(valid_csv_data):
-    """
-    Tests whether when calling the process method, all processes occur correctly:
-    objects created len; object instance type; data cleaning;
-    conversion of monetary values; conversion to datetime;
-    columns to lower case; suboject customer created;
-
-    Args:
-        valid_csv_data (str): fixture with valid csv data
-    """
-    csv_buffer = io.StringIO(valid_csv_data)
+def test_successful_processing(buy_orders_valid_csv_data):
+    """Tests that a valid CSV is processed correctly, yielding the expected number of objects."""
+    csv_buffer = io.StringIO(buy_orders_valid_csv_data)
     adapter = MgtBuyOrdersCsvAdapter(csv_buffer)
 
     validated_data = adapter.process()
-    order1 = validated_data[0]
-    order2 = validated_data[1]
 
-    # objects len generated and if is correct instance
-    validated_objects_len = 2
-    assert len(validated_data) == validated_objects_len
+    buy_orders_len = 2
+    assert len(validated_data) == buy_orders_len
     assert isinstance(validated_data[0], BuyOrderReportData)
 
-    # validated order data
-    assert order1.order_number == '507943839'
-    assert order1.status == 'cancelado'
-    assert order1.tracking_code == 'TRACK123'
 
-    # order currency conversion to decimal
-    assert order1.shipping_amount == Decimal('23.24')
-    assert order1.discount == Decimal('0.00')
-    assert order1.total_amount == Decimal('85.46')
+def test_buy_order_data(validated_buy_orders):
+    order = validated_buy_orders[0]
 
-    # date conversion
-    assert order1.order_date == datetime(2025, 4, 10, 22, 1, 33)
-
-    # customer data
-    assert order1.first_name == 'luiza'
-    assert order1.last_name == 'correia'
-    assert order1.email == 'luigi32@viana.br'
-    assert order1.cpf == '09176482340'
-    assert order1.phone == '7163794026'
-
-    # mapping
-    assert order1.payment_type == 'boleto bancário'
-
-    # second order data
-    assert order2.order_number == '414797776'
-    assert order2.status == 'entregue'
-    assert order2.payment_type == 'cartão de crédito'
-    assert order2.first_name == 'rodrigo'
+    assert order.order_number == '507943839'
+    assert order.status == 'cancelado'
+    assert order.tracking_code == 'TRACK123'
 
 
-def test_empty_strings_converted_to_none(valid_csv_data):
+def test_currency_columns_conversion(validated_buy_orders):
+    order = validated_buy_orders[0]
+
+    assert order.shipping_amount == Decimal('23.24')
+    assert order.discount == Decimal('0.00')
+    assert order.total_amount == Decimal('85.46')
+
+
+def test_datetime_columns_conversion(validated_buy_orders):
+    order = validated_buy_orders[0]
+
+    assert order.order_date == datetime(2025, 4, 10, 22, 1, 33)
+
+
+def test_customer_data(validated_buy_orders):
+    order = validated_buy_orders[0]
+
+    assert order.first_name == 'luiza'
+    assert order.last_name == 'correia'
+    assert order.email == 'luigi32@viana.br'
+    assert order.cpf == '09176482340'
+    assert order.phone == '7163794026'
+
+
+def test_columns_remapping(validated_buy_orders):
+    order = validated_buy_orders[0]
+
+    assert order.payment_type == 'boleto bancário'
+
+
+def test_empty_strings_converted_to_none(buy_orders_valid_csv_data):
     """Checks if empty strings are converted to None after cleaning process
 
     Args:
-        valid_csv_data (_type_): fixture with valid csv data
+        buy_orders_valid_csv_data (_type_): fixture with valid csv data
     """
-    csv_buffer = io.StringIO(valid_csv_data)
+    csv_buffer = io.StringIO(buy_orders_valid_csv_data)
     adapter = MgtBuyOrdersCsvAdapter(csv_buffer)
 
     validated_data = adapter.process()
@@ -111,29 +82,29 @@ def test_file_not_found_raises_error():
         MgtBuyOrdersCsvAdapter('inexistent/csv_path.csv')
 
 
-def test_invalid_data_raises_validation_error(data_with_missing_required_field):
+def test_invalid_data_raises_validation_error(buy_orders_data_with_missing_required_field):
     """
     Checks whether a ValueError from Pydantic ValidatioError when a field is missing
 
     Args:
-        data_with_missing_required_field (str): fixture with data missing one field
+        buy_orders_data_with_missing_required_field (str): fixture with data missing one field
     """
-    csv_buffer = io.StringIO(data_with_missing_required_field)
+    csv_buffer = io.StringIO(buy_orders_data_with_missing_required_field)
     adapter = MgtBuyOrdersCsvAdapter(csv_buffer)
 
     with pytest.raises(ValueError, match='Pydantic validation error'):
         adapter.process()
 
 
-def test_empty_or_header_only_file_returns_empty_list(header_only_csv_data):
+def test_empty_or_header_only_file_returns_empty_list(buy_orders_header_only_csv_data):
     """
     tests whether an empty list is returned when csv has no data
     other than the headers
 
     Args:
-        header_only_csv_data (_type_): empty data csv fixture, only headers
+        buy_orders_header_only_csv_data (_type_): empty data csv fixture, only headers
     """
-    csv_buffer = io.StringIO(header_only_csv_data)
+    csv_buffer = io.StringIO(buy_orders_header_only_csv_data)
     adapter = MgtBuyOrdersCsvAdapter(csv_buffer)
 
     validated_data = adapter.process()
--- src/reports/tests/mgt/test_adapter_customers_csv.py
+++ src/reports/tests/mgt/test_adapter_customers_csv.py
@@ -1,6 +1,5 @@
 import io
 from datetime import datetime
-from typing import List
 
 import pytest
 
@@ -8,44 +7,9 @@ from mgt.schemas.customers_report_schema import CustomerReportData
 from reports.adapters.mgt.customers_csv_adapter import MgtCustomersCsvAdapter
 
 
-@pytest.fixture
-def valid_csv_data() -> str:
-    """Returns a multi-line string with valid customer report data."""
-    return (
-        '"Créditos / Vale Presentes",ID,Nome,E-mail,Grupo,Telefone,CEP,País,Estado,"Cliente Desde"\n'  # noqa: E501
-        '"R$0,00",189117,"Júlya Viana",julyaviana23@gmail.com,Comum,,,,,"14/07/2025 11:04:50"\n'  # noqa: E501
-        '"R$0,00",189111,"Sandra Rodrigues da Silva",sandraritasilva2064@gmail.com,Comum,"(62) 9160-5830",74911-110,Brasil,GO,"14/07/2025 10:43:22"\n'  # noqa: E501
-        '"R$0,00",189099,"Karla karoline de Jesus cerqueira",karlla.fsa20@gmail.com,Comum," - ",44002-365,Brasil,BA,"14/07/2025 09:14:50"\n'  # noqa: E501
-        '"R$0,00",189106,"MARIA HELENA RIBEIRO BRISTOTTI",m.lu.bristotti@gmail.com,Comum,,,,,"14/07/2025 10:01:53"\n'  # noqa: E501
-    )
-
-
-@pytest.fixture
-def validated_customers(valid_csv_data) -> List[CustomerReportData]:
-    csv_buffer = io.StringIO(valid_csv_data)
-    adapter = MgtCustomersCsvAdapter(csv_buffer)
-
-    return adapter.process()
-
-
-@pytest.fixture
-def header_only_csv_data() -> str:
-    """Returns the headers for the customer report CSV."""
-    return '"Créditos / Vale Presentes",ID,Nome,E-mail,Grupo,Telefone,CEP,País,Estado,"Cliente Desde"\n'  # noqa: E501
-
-
-@pytest.fixture
-def data_with_missing_required_field() -> str:
-    """Returns a CSV row with a required field (Nome) missing."""
-    return (
-        '"Créditos / Vale Presentes,ID,Nome,E-mail,Grupo,Telefone,CEP,País,Estado,"Cliente Desde"\n'  # noqa: E501
-        '"R$0,00",189117,,julyaviana23@gmail.com,Comum,,,,,"14/07/2025 11:04:50"\n'
-    )
-
-
-def test_successful_processing(valid_csv_data):
+def test_successful_processing(customers_valid_csv_data):
     """Tests that a valid CSV is processed correctly, yielding the expected number of objects."""
-    csv_buffer = io.StringIO(valid_csv_data)
+    csv_buffer = io.StringIO(customers_valid_csv_data)
     adapter = MgtCustomersCsvAdapter(csv_buffer)
 
     validated_data = adapter.process()
@@ -95,9 +59,9 @@ def test_empty_optional_fields_is_none(validated_customers):
     assert customer.country is None
 
 
-def test_just_headers_csv_returns_empty_list(header_only_csv_data):
+def test_just_headers_csv_returns_empty_list(customers_header_only_csv_data):
     """Tests that processing a CSV with only a header row returns an empty list."""
-    csv_buffer = io.StringIO(header_only_csv_data)
+    csv_buffer = io.StringIO(customers_header_only_csv_data)
     adapter = MgtCustomersCsvAdapter(csv_buffer)
 
     processed_data = adapter.process()
@@ -105,9 +69,9 @@ def test_just_headers_csv_returns_empty_list(header_only_csv_data):
     assert processed_data == []
 
 
-def test_invalid_data_raises_error(data_with_missing_required_field):
+def test_invalid_data_raises_error(customers_data_with_missing_required_field):
     """Tests that a ValueError is raised when a required field is missing from the data."""
-    csv_buffer = io.StringIO(data_with_missing_required_field)
+    csv_buffer = io.StringIO(customers_data_with_missing_required_field)
     adapter = MgtCustomersCsvAdapter(csv_buffer)
 
     with pytest.raises(ValueError, match='Pydantic validation error'):
