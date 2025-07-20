7bf5ca0e6b2e68886a8f867ece2391e216ecee88
kaironn2 <jonathas_oliveira17@hotmail.com>
2025-07-19 21:32:20 -0300

    fix (urls): versioning params fixed

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/<str:version>/reports/', include('reports.api.urls')),
+    re_path(r'^api/(?P<version>(v1))/', include('reports.api.urls')),
 ]
 
 if settings.DEBUG:
