37cba115998df5d0cd31e5d544a9a1585066c0eb
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-08-21 16:50:30 -0300

    feat: logout route created to blacklist refresh tokens

--- src/auth/api/views.py
+++ src/auth/api/views.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import UserCreateSerializer
 
@@ -8,3 +12,25 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = UserCreateSerializer
+
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data.get('refresh')
+            if not refresh_token:
+                return Response(
+                    {'detail': 'Refresh token requerido.'}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(
+                {'detail': 'Logout realizado com sucesso.'}, status=status.HTTP_205_RESET_CONTENT
+            )
+        except TokenError:
+            return Response(
+                {'detail': 'Token inválido ou já expirado.'}, status=status.HTTP_400_BAD_REQUEST
+            )
