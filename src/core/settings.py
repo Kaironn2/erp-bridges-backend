25d998bf8c1c0c1f9f83f7b15ab460a255675b04
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-08-23 11:48:04 -0300

    chore: add django-cors-headers

--- src/core/settings.py
+++ src/core/settings.py
@@ -29,12 +29,24 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'django_celery_results',
+    'corsheaders',
 
     'mgt',
     'reports',
 ]
 
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
