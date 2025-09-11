da444866cd6266cbfa26755f64da90813874455e
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-09-11 17:40:59 -0300

    feat: ecs app

---
+++ src/ecs/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from mgt.models import BuyOrder
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    cnpj = models.CharField(max_length=100, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class BuyOrderEcs(models.Model):
+    ecs_order_number = models.CharField(max_length=50, unique=True)
+    ecs_order_id = models.CharField(max_length=50, unique=True)
+    buy_order = models.OneToOneField(BuyOrder, on_delete=models.CASCADE, related_name='ecs')
+    recipient_name = models.CharField(max_length=100)
+    recipient_city = models.CharField(max_length=100)
+    recipient_state = models.CharField(max_length=50)
+    recipient_zip_code = models.CharField(max_length=50)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT, related_name='ecs')
+    coupon = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.ecs_order_number
+
+    class Meta:
+        verbose_name = 'Pedido ECS'
+        verbose_name_plural = 'Pedidos ECS'
