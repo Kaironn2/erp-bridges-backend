5396dc8b355825995220dbe349d72588ffdf2dbc
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-09-12 17:43:06 -0300

    chore: buy_order_ecs renamed to EcsOrder

--- src/ecs/models.py
+++ src/ecs/models.py
@@ -11,14 +11,16 @@ class Company(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class BuyOrderEcs(models.Model):
+class EcsOrder(models.Model):
     ecs_order_number = models.CharField(max_length=50, unique=True)
     ecs_order_id = models.CharField(max_length=50, unique=True)
     buy_order = models.OneToOneField(BuyOrder, on_delete=models.CASCADE, related_name='ecs')
+    payment_date = models.DateField(blank=True, null=True)
     recipient_name = models.CharField(max_length=100)
     recipient_city = models.CharField(max_length=100)
     recipient_state = models.CharField(max_length=50)
     recipient_zip_code = models.CharField(max_length=50)
+    ecs_delivery_date = models.DateTimeField(blank=True, null=True)
     company = models.ForeignKey(Company, on_delete=models.PROTECT, related_name='ecs')
     coupon = models.CharField(max_length=100)
 
