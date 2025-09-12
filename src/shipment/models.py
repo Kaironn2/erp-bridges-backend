880800972c2e0d8a04294dbd92fd48fdbc3e0f26
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-09-12 17:43:34 -0300

    chore: add text choices on ShippingType and Shipment model adjusts

--- src/shipment/models.py
+++ src/shipment/models.py
@@ -3,8 +3,16 @@ from django.db import models
 from mgt.models import BuyOrder
 
 
-class ShippingMethod(models.Model):
+class ShippingType(models.Model):
+    class ShippingTypeChoices(models.TextChoices):
+        ENTREGA = 'entrega', 'Entrega'
+        RETIRADA = 'retirada', 'Retirada'
+
     name = models.CharField(unique=True)
+    type = models.CharField(
+        max_length=10,
+        choices=ShippingTypeChoices.choices,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -12,22 +20,18 @@ class ShippingMethod(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = 'Método de Entrega'
-        verbose_name_plural = 'Métodos de Entrega'
+        verbose_name = 'Tipo de Entrega'
+        verbose_name_plural = 'Tipos de Entrega'
 
 
 class Shipment(models.Model):
     buy_order = models.OneToOneField(BuyOrder, on_delete=models.CASCADE, related_name='shipment')
-
-    mgt_deadline = models.DateField()
-    deadline = models.DateField()
+    deadline_days = models.IntegerField()
     tracking_code = models.CharField(max_length=100, null=True, blank=True)
-    expedition_date = models.DateField(null=True, blank=True)
-    expedition_method = models.CharField(max_length=100, null=True, blank=True)
     pickup_date = models.DateField(null=True, blank=True)
     delivered_at = models.DateField(null=True, blank=True)
-    shipping_method = models.ForeignKey(
-        ShippingMethod, on_delete=models.PROTECT, related_name='shipment'
+    shipping_type = models.ForeignKey(
+        ShippingType, on_delete=models.PROTECT, related_name='shipment'
     )
     shipping_city = models.CharField(max_length=100)
     shipping_state = models.CharField(max_length=50)
@@ -37,7 +41,7 @@ class Shipment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.shipping_method} - {self.tracking_code or "no tracking code"}'
+        return f'Pedido {self.buy_order.order_number} - {self.tracking_code or "no tracking code"}'
 
     class Meta:
         verbose_name = 'Entrega'
