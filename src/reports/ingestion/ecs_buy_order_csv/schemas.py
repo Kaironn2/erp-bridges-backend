4842de5013014dab6ba458670f97ed40c83fe21c
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-09-18 17:19:48 -0300

    feat: ecs buy order csv extractor and transformer

---
+++ src/reports/ingestion/ecs_buy_order_csv/schemas.py
@@ -0,0 +1,14 @@
+COLUMN_ALIASES = {
+    'Numero da Ordem': 'order_number',
+    'Número do Pedido': 'ecs_order_number',
+    'ID': 'ecs_order_id',
+    'Observação Interna': 'internal_note',
+    'Data do Pagamento': 'payment_date',
+    'Nome do contato': 'recipient_name',
+    'CEP do contato': 'recipient_zip_code',
+    'Cidade do contato': 'recipient_city',
+    'UF do Contato': 'recipient_state',
+    'Data de Entrega': 'ecs_delivery_date',
+    'Transportadora': 'carrier',
+    'Forma Frete': 'carrier_type',
+}
